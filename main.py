9cd6b000eb75e0e38a160e2571837c6110d570ce
naltukhov <naltukhov@ourgapps.com>
2024-08-01 18:16:56 +0400

    add logic base on priorities

--- main.py
+++ main.py
@@ -4,10 +4,51 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import requests
 
 
-def split_students_into_programs(df: pd.DataFrame):
-    pass
+DIMA_ID = "175-199-309 03"
+
+
+def make_tg_post(title, body):
+    tg_api_token = "7060682166:AAFAjE8y8BDGikZ7otEoXC7MdnxmEtHGcEk"
+    post_text = f"""<strong>{title}</strong>\n{body}"""
+
+    response = requests.get(f"https://api.telegram.org/bot{tg_api_token}/sendMessage",
+                            params={"chat_id": "@dimon_admission",
+                                    "text": post_text,
+                                    "parse_mode": "HTML"})
+    if response.status_code == 200:
+        return post_text
+
+
+def split_students_into_programs(sdf_: pd.DataFrame, pdf_: pd.DataFrame) -> pd.DataFrame:
+    """
+    Split students into programs using priorities
+
+    sdf - students dataframe
+    pdf - programs dataframe
+    """
+    sdf = sdf_.copy()
+    pdf = pdf_.copy()
+
+    free_places = pdf[["program_code", "places"]].set_index("program_code").to_dict()["places"]
+    split_result = []
+
+    sdf = sdf.sort_values(by=["score", "priority"], ascending=(False, True))
+
+    while (len(sdf) > 0) and (sum(free_places.values()) > 0):
+        top = sdf.head(1).to_dict("records")[0]
+
+        if free_places[top["program_code"]] > 0:
+            free_places[top["program_code"]] -= 1
+            top["free_places_after"] = free_places[top["program_code"]]
+            split_result.append(top)
+            sdf = sdf.loc[sdf.student_id != top["student_id"]]
+        else:
+            sdf = sdf.iloc[1:]
+
+    return pd.DataFrame(split_result)
 
 
 def request_students_by_url(program_url: str):
@@ -49,11 +90,37 @@ def request_students_by_url(program_url: str):
     return df
 
 
-if __name__ == '__main__':
-    students_data = []
-    urls_df = pd.read_csv("database/urls", sep=' ')
-    for program_code, url in urls_df.values:
+def get_students_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Return students data for all programs
+    """
+    st_df = []
+    for program_code, url in df[["program_code", "url"]].values:
         url_students_df = request_students_by_url(url)
         url_students_df["program_code"] = program_code
-        students_data.append(url_students_df)
-    students_data_df = pd.concat(students_data, axis=0)
+        st_df.append(url_students_df)
+    students_data_df = pd.concat(st_df, axis=0)
+    return students_data_df
+
+
+if __name__ == '__main__':
+    programs_df = pd.read_csv("database/urls_fkti.csv")
+    students_df = get_students_data(programs_df)  # to get new info
+    # students_df = pd.read_csv("database/test_students.csv")  # to get mocked data
+
+    result_df = split_students_into_programs(students_df, programs_df)
+
+    # get dima position by DIMA_ID
+    dima_program_info = result_df.loc[result_df.student_id == DIMA_ID].to_dict("records")[0]
+
+    # send info to tg chat
+    program_name, program_code, all_places = programs_df.loc[
+        programs_df.program_code == dima_program_info["program_code"],
+        ["program_name", "program_code", "places"]
+    ].values[0]
+    
+    position_num = all_places - dima_program_info["free_places_after"]
+    
+    title = f'{program_name} ({program_code})'
+    body = f'{position_num}/{all_places}'
+    make_tg_post(title=title, body=body)
